a3e790013c4ddf7711524959dd7361a2e0d6f8ab
Anton <a.begehr@gmail.com>
2021-05-16 15:47:28 +0200

    install and configure beamer
    https://pub.dev/packages/beamer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,21 +9,70 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Beamer Deep Linking',
-      home: ListPage(),
+      routeInformationParser: BeamerRouteInformationParser(),
+      routerDelegate: BeamerRouterDelegate(
+        locationBuilder: SimpleLocationBuilder(
+          routes: {
+            '/': (context) => HomeScreen(),
+            '/books': (context) => BooksScreen(),
+            '/books/:id': (context) => BookDetailsScreen()
+          },
+        ),
+      ),
     );
   }
 }
 
-class ListPage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Beamer Deep Linking"),
       ),
-      body: Container(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Beamer.of(context).beamToNamed('/books');
+          },
+          child: Text("Go to books"),
+        ),
+      ),
+    );
+  }
+}
+
+class BooksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Beamer Deep Linking - Books"),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          onTap: () {
+            Beamer.of(context).beamToNamed('/books/$index');
+          },
+          leading: Icon(CupertinoIcons.book),
+          title: Text("Book $index"),
+        ),
+      ),
+    );
+  }
+}
+
+class BookDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final id = Beamer.of(context).currentLocation.state.pathParameters['id'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Beamer Deep Linking – Book $id"),
+      ),
+      body: Center(child: Text("This is book #$id.")),
     );
   }
 }
